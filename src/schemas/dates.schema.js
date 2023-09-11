208e3a9991a93b1730b6b167f4c60e9d7bacedaa
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-11 14:32:27 -0300

    fix: date validation

--- src/schemas/dates.schema.js
+++ src/schemas/dates.schema.js
@@ -2,7 +2,7 @@ import joiBase from "joi";
 import joiDate from "@joi/date";
 const joi = joiBase.extend(joiDate);
 
-export const dateQuerySchema = joi.object({
+export const dateSchema = joi.object({
     smallDate: joi.date().format("DD-MM-YYYY").required(),
     bigDate: joi.date().format("DD-MM-YYYY").required(),
 });

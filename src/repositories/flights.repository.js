bd8ed977b3a41c6b7772669bbebdb227bfecf9e8
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-11 17:02:21 -0300

    feat: select function

--- src/repositories/flights.repository.js
+++ src/repositories/flights.repository.js
@@ -10,5 +10,48 @@ async function selectById(id) {
     return result.rows[0];
 };
 
-const flightsRepository = { insert, selectById };
+async function select() {
+    let query = `
+    SELECT 
+        flights.id, 
+        origin.name AS origin, 
+        destination.name AS destination, 
+        TO_CHAR(date, 'DD-MM-YYYY') AS date
+    FROM flights
+    JOIN cities AS origin ON flights.origin = origin.id
+    JOIN cities AS destination ON flights.destination = destination.id`;
+
+    const queryParams = [];
+
+    if (origin) {
+        query += ` WHERE origin.name = $${queryParams.length + 1}`;
+        queryParams.push(origin);
+    }
+
+    if (destination) {
+        if (query.includes("WHERE")) {
+            query += `AND destination.name = $${queryParams.length + 1}`;S
+        } else {
+            query += ` WHERE destination.name = $${queryParams.length + 1}`;
+        }
+        queryParams.push(destination);
+    }
+
+    if (smallDateFormated && bigDateFormated) {
+        if (query.includes("WHERE")) {
+            query += `AND flights.date BETWEEN TO_DATE($${queryParams.length+1}, 'YYYY-MM-DD') AND TO_DATE($${queryParams.length + 2}, 'YYYY-MM-DD')`;
+        } else {
+            query += ` WHERE flights.date BETWEEN TO_DATE($${queryParams.length+1}, 'YYYY-MM-DD') AND TO_DATE($${queryParams.length + 2}, 'YYYY-MM-DD')`;
+        }
+        queryParams.push(smallDateFormated);
+        queryParams.push(bigDateFormated);
+    }
+
+    query += ` ORDER BY flights.date`;
+
+    const flights = await db.query(query, queryParams);
+    return flights.rows;
+};
+
+const flightsRepository = { insert, selectById, select };
 export default flightsRepository;

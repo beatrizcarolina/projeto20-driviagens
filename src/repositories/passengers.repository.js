4d6709e7fb9725290c572923ba358ce7b14af930
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-11 17:02:48 -0300

    feat: select flights function

--- src/repositories/passengers.repository.js
+++ src/repositories/passengers.repository.js
@@ -9,5 +9,28 @@ async function selectById(id) {
     return result.rows[0];
 };
 
-const passengersRepository = { insert, selectById };
+async function selectFlights(name) {
+    let query = `
+    SELECT 
+        (passengers."firstName" || ' ' || passengers."lastName") AS passenger, 
+        CAST(COUNT(travels."passengerId") AS INTEGER) AS travels
+    FROM passengers
+    LEFT JOIN travels ON passengers.id = travels."passengerId"`;
+
+    const queryParams = [];
+
+    if (name) {
+        query += ` 
+            WHERE passengers."firstName" ILIKE $${queryParams.length + 1} 
+            OR passengers."lastName" ILIKE $${queryParams.length + 1}`;
+        queryParams.push(`%${name}%`);
+    }
+
+    query += " GROUP BY passengers.id ORDER BY travels DESC";
+
+    const flightsPerPassenger = await db.query(query, queryParams);
+    return flightsPerPassenger.rows;
+};
+
+const passengersRepository = { insert, selectById, selectFlights };
 export default passengersRepository;

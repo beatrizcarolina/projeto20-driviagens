dc87f5443c5e7919846f5d4695329bcbd6d7d34f
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-11 15:52:22 -0300

    feat: post route

---
+++ src/routes/flights.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import validateSchema from "../middlewares/validateSchema.js";
+import { flightSchema } from "../schemas/flights.schema.js";
+import flightsController from "../controllers/flights.controller.js";
+
+const flightsRouter = Router();
+flightsRouter.post("/flights", validateSchema(flightSchema), flightsController.register);
+flightsRouter.get("/flights");
+
+export default flightsRouter;

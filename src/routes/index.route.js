ffffa63b2ac525b3d76cb22d19e80adb18e83270
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-11 14:31:52 -0300

    feat: config cities, flights and travels routes

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import passengerRouter from "./passengers.route.js";
+import citiesRouter from "./cities.route.js";
+import flightsRouter from "./flights.route.js";
+import travelsRouter from "./travels.route.js";
 
 const router = Router();
 router.use(passengerRouter);
-
+router.use(citiesRouter);
+router.use(flightsRouter);
+router.use(travelsRouter);
 
 export default router;

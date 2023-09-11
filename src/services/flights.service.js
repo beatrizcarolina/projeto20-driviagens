ff50f82358937d2ceccd5d8cbac1c5e8e8b5403e
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-11 17:01:14 -0300

    feat: get flight function

--- src/services/flights.service.js
+++ src/services/flights.service.js
@@ -1,5 +1,5 @@
 import flightsRepository from "../repositories/flights.repository.js";
-import { cityValidation } from "../utils/validations.js";
+import { cityValidation, dateValidation } from "../utils/validations.js";
 
 async function create(origin, destination, date) {
     const validOrigin = await cityValidation(origin);
@@ -21,5 +21,40 @@ async function create(origin, destination, date) {
     return await flightsRepository.insert(origin, destination, rightDate);
 };
 
-const flightsService = { create };
+function verifyDates(smallDate, bigDate) {
+    const validDate = dateValidation(smallDate, bigDate);
+    if (validDate) {
+        throw { type: "unprocessable", message: validDate };
+    }
+
+    if (new Date(smallDateFormated) > new Date(bigDateFormated)) {
+        throw { type: "unprocessable", message: "Smaller date cannot be greater than bigger date" };
+    }
+}
+
+async function getFlight(origin, destination, smallDate, bigDate) {
+    const validOrigin = await cityValidation(origin);
+    const validDestination = await cityValidation(destination);
+
+    if (!validOrigin || !validDestination) {
+        throw { type: "notFound", message: "City not found" };
+    }
+
+    if (origin === destination) {
+        throw { type: "conflict", message: "Origin and destination cannot be the same" };
+    }
+
+    if ((smallDate || bigDate) && !((smallDate && bigDate))) {
+        throw { type: "unprocessable", message: "Both dates must be passed together" };
+    }
+
+    if( smallDate || bigDate) {
+        verifyDates(smallDate, bigDate);
+    }
+
+    const flights = await flightsRepository.select(origin, destination, smallDate, bigDate);
+    return flights;
+};
+
+const flightsService = { create, getFlight };
 export default flightsService;

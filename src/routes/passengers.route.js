fe11c4cc0af02bb32abe01a2b0b81c49cdc93b90
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-11 16:59:11 -0300

    feat: get route

--- src/routes/passengers.route.js
+++ src/routes/passengers.route.js
@@ -5,6 +5,6 @@ import passengersController from "../controllers/passengers.controller.js";
 
 const passengerRouter = Router();
 passengerRouter.post("/passengers", validateSchema(passengerSchema), passengersController.register);
-passengerRouter.get("/passengers/travels");
+passengerRouter.get("/passengers/travels", passengersController.get);
 
 export default passengerRouter;
